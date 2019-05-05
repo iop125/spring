036f0b0d3f33fbf6d1859ae22cd5b0d8c0ad1654
缪宇 <miaoyu1@jd.com>
2019-05-05 18:20:16 +0800

    拦截器

--- src/main/java/com/my/demo/controller/TestController.java
+++ src/main/java/com/my/demo/controller/TestController.java
@@ -9,9 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
     @Value("${plugin-cente.jdbc.password}")
     private String s;
+
     @RequestMapping("/ss")
     public String ss(){
-        System.out.println("----------");
+        System.out.println("--------ss--");
+        return "index";
+    }
+    @RequestMapping("/sss")
+    public String sss(){
+        System.out.println("---------sss-");
         return "index";
     }
 }
